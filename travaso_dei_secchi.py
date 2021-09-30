37a47e09aa11d35c52e44e77acf86c45c2206775
dveni <28764301+dveni@users.noreply.github.com>
2021-09-30 16:31:38 +0200

    Code commented

--- travaso_dei_secchi.py
+++ travaso_dei_secchi.py
@@ -17,27 +17,48 @@ left
 destination bucket doesn’t have enough capacity to accept the entire content of the
 starting bucket 
 """
+
+"""
+ASSUMPTIONS:
+- I suppose every node has only one parent, althoug most of the nodes can be reached
+ from multiple paths. So we do not find the most short, just a path to the goal node.
+- Assumed there are finite states
+- Only 3 buckets
+"""
+
 PARENT_NODE = 0
 EXPLORED_NODE = 1
 NODE_ID = 2
 
+
 class State:
+    """
+    Class representating the volume levels of each bucket in a specific instant.
+    """
     def __init__(self, bucketA, bucketB, bucketC) -> None:
         self.bucketA = bucketA
         self.bucketB = bucketB
         self.bucketC = bucketC
         if not isValidState(self):
             raise ValueError('All initial volumes must be less than the maximun volumes')
+    
     def getBuckets(self):
         return (self.bucketA, self.bucketB, self.bucketC)
+    
     def getBucketsLevels(self):
         return (self.bucketA.level, self.bucketB.level, self.bucketC.level)
+    
     def numOfBuckets(self):
         return len(self.getBuckets())
+    
     def __str__(self) -> str:
         return f"{self.bucketA} | {self.bucketB} | {self.bucketC}"
 
+
 class Bucket:
+    """
+    Class representing a bucket.
+    """
     def __init__(self, name, capacity, level) -> None:
         self.name = name
         self.capacity = capacity
@@ -45,12 +66,16 @@ class Bucket:
     def __str__(self) -> str:
         #return f"{self.name}. Capacity: {self.capacity}. Level: {self.level}"
         return f"{self.level}"
+    def canGive(self) -> bool:
+        return self.level > 0
 
 
 
 def generatePossibleStates(initialState):
     """
-    O(n*(n-1)) -> n=3 buckets
+    Given a state, generates all possible future states.
+    
+    Complexity: O(n*(n-1)) -> n=3 buckets
     """
     possibleStates = []
 
@@ -58,7 +83,7 @@ def generatePossibleStates(initialState):
         for j in range(1, initialState.numOfBuckets()):
             newState = copyState(initialState)
             buckets = newState.getBuckets()
-            if canGive(buckets[i]):
+            if buckets[i].canGive():
                 pourWater(buckets[i],buckets[(i+j)%3])
                 if isValidState(newState):
                     possibleStates.append(newState)
@@ -67,12 +92,17 @@ def generatePossibleStates(initialState):
 
 def pourWater(b1, b2):
     """
-    O(1)
+    Pour water from bucket b1 into bucket b2 according to given rules:
+    A given pouring:
+    - Will completely empty the starting bucket if the destination bucket has enough capacity
+    left
+    - Will partially empty the starting bucket (until the destination bucket is full), if the
+    destination bucket doesn’t have enough capacity to accept the entire content of the
+    starting bucket 
+    
+    Complexity: O(1)
     """
-    # Pour water from bucket b1 into bucket b2
 
-    # Test both objects are buckets
-    # Test edge cases
     if b1.level == 0: return b1, b2
     
     freeRoomB2 = max(b2.capacity - b2.level,0)
@@ -92,8 +122,6 @@ def copyState(state):
 
     return State(bA,bB,bC)
 
-def canGive(bucket):
-    return bucket.level > 0
 
 def isValidState(state):
     return not(state.bucketA.level > state.bucketA.capacity or
@@ -102,20 +130,23 @@ def isValidState(state):
  
 def compareStates(stateA, stateB):
     """
-    O(1)
+    Two states are equal if they have the same volume level in all buckets.
+    Complexity: O(1)
     """
     return hash(stateA.getBucketsLevels()) == hash(stateB.getBucketsLevels())
 
 def getUnexploredNodes(table):
     """
-    O(n) n -> #nodes in table
+    Get nodes not preivously explored.
+    Complexity: O(n) n -> #nodes in table
     """
     unexplored = [item for item in table if not table[item][EXPLORED_NODE]]
     return unexplored
 
 def stateExist(state, table, nodes):
     """
-    O(1)
+    Check if a state is already stored in the table
+    Complexity: O(1)
     See https://stackoverflow.com/questions/17539367/python-dictionary-keys-in-complexity
     """
     return hash(state.getBucketsLevels()) in table
@@ -124,9 +155,14 @@ def stateExist(state, table, nodes):
 
 def getGoalStatePath(initialState, goalState):
     """
+    Get the sequence of pourings from initialState to goalState, if possible
+
     O(n^3)
     statesUntilGoalState * unexploredNodes * possibleStates
     For this problem, max = 8*3*5 = 120
+
+    As both unexplored nodes and possible states for each node are low (<5), 
+    we could consider O(n) complexity for 3 buckets
     """
     hashTable = {hash(initialState.getBucketsLevels()): ["root", False, 0]}
     nodes = [initialState]
@@ -142,7 +178,7 @@ def getGoalStatePath(initialState, goalState):
         """
         O(n^2) 
         unexploredNodes * possibleStates
-        Typically, there are between 2-3 unexplored nodes
+        Typically, there are between 2-3 unexplored nodes and <5 possible states.
         """
         for nodeId in unexploredNodes:
 
@@ -151,7 +187,7 @@ def getGoalStatePath(initialState, goalState):
             hashTable[nodeId][EXPLORED_NODE] = True
             # O(n) n-> #possible states (empirically, max 5)
             for state in states:
-                # O(n)
+                # O(1)
                 if stateExist(state, hashTable,nodes): continue
                 # O(1)
                 stateFound = compareStates(state, goalState)
@@ -168,17 +204,20 @@ def getGoalStatePath(initialState, goalState):
         while previousNode != "root":
             path.append(nodes[hashTable[previousNode][NODE_ID]])
             previousNode = hashTable[previousNode][PARENT_NODE]
-
+        print("Sequence of pourings:")
+        print("A | B | C")
         for node in path[::-1]:
             print(node)
 
 if __name__== "__main__":
     
+    # Define initial state
     bA = Bucket("Bucket A", 8, 8)
     bB = Bucket("Bucket B", 5, 0)
     bC = Bucket("Bucket C", 3, 0)
     initialState = State(bA, bB, bC)
     
+    # Define goal state
     goalA = Bucket("Bucket A", 8, 4)
     goalB = Bucket("Bucket B", 5, 4)
     goalC = Bucket("Bucket C", 3, 0)
@@ -187,12 +226,11 @@ if __name__== "__main__":
     getGoalStatePath(initialState, goalState)
     
 
-
-
-
-# I suppose every node has only one parent,
-# althoug much of the nodes can be reacher from multiple paths so we do not find the most short, just a path
-
-# Assume there are finite states
-
-# Only 3 buckets
+    # 8 | 0 | 0
+    # 3 | 5 | 0
+    # 3 | 2 | 3
+    # 6 | 2 | 0
+    # 6 | 0 | 2
+    # 1 | 5 | 2
+    # 1 | 4 | 3
+    # 4 | 4 | 0
